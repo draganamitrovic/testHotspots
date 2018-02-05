c19810a3c7faba52b64671df3401d3d396b33d04
Dusan Zivkovic <dusan.zivkovic@cadev.rs>
2018-02-05 11:16:04 +0100

    metoda nesto

--- App.js
+++ App.js
@@ -1,31 +1,25 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, TouchableOpacity, Text } from 'react-native';
 
 export default class FullImage extends Component {
 
   getPosistionsFromJSON = () => {
     const fileJSON = require('./img/file.json');
     console.log(fileJSON);
-    // const fileParsed = JSON.parse(fileJSON);
-    fileJSON.spots.forEach(spot => {
-      console.log(spot);
-    })
+    spots = fileJSON.spots.map((spot, i) => <Text key={i}>Dugme</Text>);
+    return spots;   
   }
 
   render() {
-    this.getPosistionsFromJSON();
     return (
       <View style={styles.mainView}>
 
         <View style={styles.body}>
 
           <View style={styles.contentContainer}>
-
-           
           
               <Image resizeMethod='resize' style={{ width: '100%', height: '100%', resizeMode: 'cover'}} source={require('./img/img.jpeg')} />
-
-        
+              {this.getPosistionsFromJSON()}       
           </View>
 
         </View>
