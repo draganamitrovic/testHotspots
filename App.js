19e871bc896b6fd38e372c4712318aa88ad98917
draganamitrovic <dragana.mitrovic.94@gmail.com>
2018-02-05 10:21:31 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+export default class FullImage extends Component {
+
+  render() {
+
+    return (
+      <View style={styles.mainView}>
+
+        <View style={styles.body}>
+
+          <View style={styles.contentContainer}>
+
+           
+          
+              <Image resizeMethod='resize' style={{ width: '100%', height: '100%', resizeMode: 'cover'}} source={require('./img/img.jpeg')} />
+
+        
+          </View>
+
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mainView: {
+    backgroundColor: 'white',
+    position: 'relative',
+    height: '100%'
+  },
+  body: {
+    height: '100%',
+  },
+  contentContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    marginBottom: 5,
+  },
+});
