f21b8f972bad904542575bfefa211dce0799b388
draganamitrovic <dragana.mitrovic.94@gmail.com>
2018-02-05 12:00:20 +0100

    dugme radi

--- App.js
+++ App.js
@@ -7,20 +7,33 @@ export default class FullImage extends Component {
     const fileJSON = require('./img/file.json');
     fromTop = 100;
     console.log(fileJSON);
-    spots = fileJSON.spots.map((spot, i) => <Text onPress={() => console.log(i)} style={{position: 'absolute', top: fromTop*i, left: 40, zIndex: 999, color: 'white'}} key={i}>Dugme</Text>);
-    return spots;   
+    spots = fileJSON.spots.map((spot, i) =>
+      <Text
+        onPress={() => console.log(i)}
+        style={
+          {
+            position: 'absolute',
+            top: (spot.y) + '%',
+            left: (spot.x) + '%',
+            zIndex: 999, color: 'white'
+          }}
+        key={i}>
+        Dugme
+         </Text>);
+    return spots;
   }
 
   render() {
+
     return (
       <View style={styles.mainView}>
 
         <View style={styles.body}>
 
           <View style={styles.contentContainer}>
-          
-              <Image resizeMethod='resize' style={{ width: '100%', height: '100%', resizeMode: 'cover'}} source={require('./img/img.jpeg')} />
-              {this.getPosistionsFromJSON()}       
+
+            <Image resizeMethod='resize' style={{ width: '100%', height: '100%', resizeMode: 'cover' }} source={require('./img/img.jpeg')} />
+            {this.getPosistionsFromJSON()}
           </View>
 
         </View>
